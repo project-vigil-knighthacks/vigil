166fcce3a1533b776fb069e701ef5c71b17ad14e
NickTheBrick04 <nickwesterfeld@gmail.com>
2026-03-08 17:52:49 -0400

    Adding improved log_collector takes in a log dir in its run line and gets watched by watchdog which continuously runs the eventhandler class when there is an update it triggers and sends information to api and prints aswell for now, imports mikes grokmoment parser wrapping around that process running in the on_modified def

--- vigil/backend/api/log_collector.py
+++ vigil/backend/api/log_collector.py
@@ -1,40 +1,29 @@
 #collect logs from various sources (e.g., system logs, application logs,
 #network logs) and store them in a centralized location for further processing and analysis
-import time # for pausing between log collection cycles
+from time import sleep # for pausing between log collection cycles
 import requests # for sending http requests to fastapi server
-import os # for log file handling
-from watchdog.events import FileSystemEvent, FileSystemEventHandler
+from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
-from grokmoment import parse_logs
+from grokmoment import parse_logs_by_excerpt
+import argparse
 
 FASTAPI_SERVER_URL = "http://localhost:8000/api/collect" # URL of the FastAPI server to send logs to
 POLL_INTERVAL = 1
 
 
-# Function continuously scans for new log input from the .log file location - Nick
-def follow(filepath: str):
-    with open(filepath, "r") as f:
-        f.seek(0,2)
-        while True:
-            line = f.readline()
-            if not line:
-                time.sleep(POLL_INTERVAL)
-                continue
-            if line:
-                yield line.strip()
+def send_to_api(events: list):
+        requests.post(FASTAPI_SERVER_URL, json=events )
 
 class eventHandler(FileSystemEventHandler):
-    def __init__(self, log: str, output: str='out.json', grok_patterns_file: str='patterns.json'):
-        self.processor = logProcessor(grok_patterns_file, output)
+    def __init__(self, log: str):
         self.log = log
 
         with open(self.log, 'r') as f:
             self.pre = [line.rstrip() for line in f] # maybe handling for very large logs
 
         print('waiting for event...')
-
-# Stolen from mikes parser because we are seperating logic in files for better architecture -nick
-
+    
+    # grabbed from mikes parser and has  been edited to seperate logic in files for better architecture -nick
     def on_modified(self, event) -> None:
         if event.src_path == f'./{self.log}':
             with open(self.log, 'r') as f:
@@ -45,13 +34,34 @@ class eventHandler(FileSystemEventHandler):
                 now = set(now) - set(self.pre)
                 now = list(now)
                 # this process will no longer call itself instead it will call the parser function using parser import
-                events = parse_logs(now)
-                f.seek(0) # go back to the beginning of the file to redefine pre
+                events = parse_logs_by_excerpt(now)
                 pre = [line.rstrip() for line in f]
                 self.pre = pre[-250:] # hold last 250 lines
-                print('waiting for event...')
+                print(events)
+                send_to_api(events)
 
-#WIP
-def send_to_api(line: str):
-    requests.post(FASTAPI_SERVER_URL, )
 
+
+def main():
+    #parse CLI
+    parser = argparse.ArgumentParser(description="Log Collection Agent and Manager")
+    parser.add_argument("-l", "--log", help="Pass log path")
+    args = parser.parse_args()
+    log = args.log
+
+    #create handler class and observer
+    handler = eventHandler(log)
+    observer = Observer()
+    # Check handle in current directory, dont check subdir
+    observer.schedule(handler, ".", recursive=False)
+    observer.start()
+    try:
+        while True:
+            sleep(1)
+    finally:
+        observer.stop()
+        observer.join()
+
+#import check
+if __name__ == "__main__":
+    main()
