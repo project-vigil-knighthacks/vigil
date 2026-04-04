29fb358229aae66c841621ec5306e930b54cc119
NickTheBrick04 <nickwesterfeld@gmail.com>
2026-04-04 00:13:24 -0400

    working MailGun test

--- backend/api/emailer.py
+++ backend/api/emailer.py
@@ -14,7 +14,7 @@ def send_simple_message():
 	api_key = os.getenv("MAILGUN_API_KEY")
 	if not api_key:
 		raise HTTPException(status_code=500, detail="MAILGUN_API_KEY not set")
-	return requests.post(
+	resp =  requests.post(
   		"https://api.mailgun.net/v3/sandbox9cbb2289c7094d94a2c44ac87d927e96.mailgun.org/messages",
   		auth=("api", api_key),
   		data={"from": "Mailgun Sandbox <postmaster@sandbox9cbb2289c7094d94a2c44ac87d927e96.mailgun.org>",
@@ -22,3 +22,8 @@ def send_simple_message():
   			"subject": "Hello Nicholas Westerfeld",
   			"text": "Congratulations Nicholas Westerfeld, you just sent an email with Mailgun! You are truly awesome!"}
 			)
+	return{
+		"status_code": resp.status_code,
+        "text": resp.text,
+    }
+
